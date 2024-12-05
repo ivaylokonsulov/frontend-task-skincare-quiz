c366db80335f808700144f6e4a2fa4c009f87cc1
Ivaylo Konsulov <konsulov.ivailo@gmail.com>
2024-12-05 14:36:09 +0200

    Removed unnecessary files/code.

---
+++ src/MainPage.js
@@ -0,0 +1,15 @@
+import mainPagePhoto from './mainPagePhoto.png';
+
+function MainPage() {
+    return (
+        <>
+        <header className="App-header">
+        <img src={mainPagePhoto} className="BackgroundImage" alt="female-legs-in-bath-with-oranges" />
+        <p>
+        </p>
+      </header>
+      </>
+    );
+  }
+
+  export default MainPage;
